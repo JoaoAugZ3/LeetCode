9bb15ff20b338a054ee152377dc30f9781311ed9
JoaoAugZ3 <133545656+JoaoAugZ3@users.noreply.github.com>
2023-10-09 11:40:54 -0300

    Delete LeetCode directory

--- LeetCode/Easy/1929/1929.py
+++
@@ -1,8 +0,0 @@
-# Concluído
-# Runtime: 68ms
-# Memory: 16.5MB
-
-class Solution:
-    def getConcatenation(self, nums: List[int]):
-        nums2 = nums
-        return nums + nums2
--- LeetCode/Easy/2235/2235.py
+++
@@ -1,7 +0,0 @@
-# Concluído
-# Runtime: 33ms
-# Memory: 16.3MB
-
-class Solution:
-    def sum(self, num1: int, num2: int) -> int:
-        return num1+num2
