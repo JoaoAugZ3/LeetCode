98a9d0d9174afcde34a425195e26a10ae9b7fdb6
JoaoAugZ3 <133545656+JoaoAugZ3@users.noreply.github.com>
2023-10-09 11:41:56 -0300

    Add files via upload

---
+++ Easy/1929/1929.py
@@ -0,0 +1,8 @@
+# Concluído
+# Runtime: 68ms
+# Memory: 16.5MB
+
+class Solution:
+    def getConcatenation(self, nums: List[int]):
+        nums2 = nums
+        return nums + nums2
---
+++ Easy/2235/2235.py
@@ -0,0 +1,7 @@
+# Concluído
+# Runtime: 33ms
+# Memory: 16.3MB
+
+class Solution:
+    def sum(self, num1: int, num2: int) -> int:
+        return num1+num2
